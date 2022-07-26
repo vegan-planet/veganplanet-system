2a5f9efb8a4ba727c0c69927da6bc11108e00d59
王小欢 <wxh_work@163.com>
2022-07-26 19:13:20 +0800

    init:初始化工程目录

---
+++ veganplanet-upms/veganplanet-upms-biz/src/main/java/com/veganplanet/system/admin/VeganplanetAdminApplication.java
@@ -0,0 +1,18 @@
+package com.veganplanet.system.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: veganplanet-system
+ * @description:
+ * @author: 王小欢
+ * @create: 2021-05-23 22:57
+ **/
+@SpringBootApplication
+public class VeganplanetAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VeganplanetAdminApplication.class, args);
+    }
+
+}
